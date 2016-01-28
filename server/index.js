b2d1f4a49dcba1f5991a8e8784f39bf648d7752f
laike <lake1355@qq.com>
2016-01-28 17:38:21 +0800

    modified directory

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
+	console.log(__filename);
   res.sendFile(__dirname+'/chat.html');
+});
+io.on('connection',function(socket){
+	  console.log('a user connected!\n');
+	  socket.on('disconnect',function(){
+	  	  console.log('a user disconnected!\n ');
+	  });
+	  socket.on('chat message',function(msg){
+	  	  console.log('message: '+msg);
+	  	  io.emit('chat message',msg);
+	  });
 });
 
 http.listen(3000, function(){
