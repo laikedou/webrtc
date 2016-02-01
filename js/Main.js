749ead6b0ec07dfc5caa66384d8cdb8396af3fc1
laike <lake1355@qq.com>
2016-02-01 16:43:13 +0800

    add the whenReady Model

--- js/Main.js
+++ js/Main.js
@@ -141,6 +141,41 @@ var Tools = (function(){
         }
     };
 }());
+//自用工具库
+var whenReady = (function(){
+    var ready = false;
+    var funcs = [];//存储函数的数组
+    function handler(e){
+        if(ready) return;
+        if(e.type === 'readystatechange' && document.readyState !== 'commplete'){
+            return;
+        }
+        for(var i=0;i<funcs.length;i++){
+              funcs[i].call(document);
+        }
+        //进行标记
+        ready  = true;
+        funcs = null;//置空
+    }
+    if(document.addEventListener){
+         document.addEventListener('DOMContentLoaded', handler,false);
+         document.addEventListener('readystatechange', handler,false);
+         window.addEventListener('load',handler,false);
+    }else{
+        //兼容IE等不支持addEventListener方法的浏览器
+        document.attachEvent('onreadystatechange',handler);
+        window.attachEvent('onload',handler);
+    }
+
+    return function isReady(f){
+
+        if(ready){
+             f.call(document);
+        }else{
+            funcs.push(f);
+        }
+    }
+}());
 //webrtc
 var webRtc = (function(options){
     var _defaultOpts={};
@@ -450,6 +485,8 @@ var webRtc = (function(options){
         }
     }
 }());
-window.onload = function(){
-    webRtc.init();
-};
+
+//当页面文档和deffer属性的script加载完毕后就可以执行webrtc这个方案 而不必等待页面asyn script和图片加载
+whenReady(function(){
+        webRtc.init();
+});
